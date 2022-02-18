a4254a57da6b15e528eb7e3db0b1c35613e25808
Anastasiya Latugovskaya <latugovskayawork@gmail.com>
2022-02-19 00:48:33 +0300

    solution hw-2

--- solution.go
+++ solution.go
@@ -20,11 +20,11 @@ const (
 
 func CalcSquare(sideLen float64, sidesNum Figure) float64 {
 	switch sidesNum {
-	case SidesCircle:
-		return math.Sqrt(3) / 4 * math.Pow(sideLen, 2)
 	case SidesTriangle:
-		return math.Pow(sideLen, 2)
+		return math.Sqrt(3) / 4 * math.Pow(sideLen, 2)
 	case SidesSquare:
+		return math.Pow(sideLen, 2)
+	case SidesCircle:
 		return math.Pi * math.Pow(sideLen, 2)
 	default:
 		return 0
